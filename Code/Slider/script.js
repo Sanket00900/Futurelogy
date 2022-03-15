f057b3a0a49e6a782da118d090e075e16754b7da
Sanket Sanjay Jadgale <sanketsjagdale999@gmail.com>
2022-03-15 18:16:24 +0530

    script added

---
+++ Code/Slider/script.js
@@ -0,0 +1,85 @@
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('load scroll',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if($(window).scrollTop() > 0){
+      $('header').addClass('sticky');
+    }else{
+      $('header').removeClass('sticky');
+    }
+
+    if($(window).scrollTop() > 0){
+      $('.scroll-top').show();
+    }else{
+      $('.scroll-top').hide();
+    }
+
+    // scroll spy 
+
+    $('section').each(function(){
+
+      let top = $(window).scrollTop();
+      let offset = $(this).offset().top - 200;
+      let id = $(this).attr('id');
+      let height = $(this).height();
+
+      if(top > offset && top < offset + height){
+        $('.navbar a').removeClass('active');
+        $('.navbar').find(`[href="#${id}"]`).addClass('active');
+      }
+
+    });
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,
+      'linear'
+    );
+
+  });
+
+});
